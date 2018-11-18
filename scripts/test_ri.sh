2b49e70d71ffced2680be3c2bd3ec368b289aa4c
Guy Mograbi <guy.mograbi@gmail.com>
2018-11-18 10:35:13 -0800

    make RI tests fail.

--- scripts/test_ri.sh
+++ scripts/test_ri.sh
@@ -7,6 +7,8 @@ nvm install
 echo decoding me.conf
 mkdir -p conf/dev
 echo TESTME_KEY=$TESTME_KEY
+
+set -e
 openssl aes-256-cbc -d -a -in ../lergo-build/scripts/testMe2.json.enc -out conf/dev/me.json -pass pass:$TESTME_KEY
 ./node_modules/.bin/grunt testBefore
 ./node_modules/.bin/grunt
