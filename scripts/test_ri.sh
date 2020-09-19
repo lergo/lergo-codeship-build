9878bed5846d0e5c7097733d73640f70559b7fa1
Jeffrey <4lergo@gmail.com>
2020-09-19 18:14:40 +0300

    add -md md5 due to openSSl issues

--- scripts/test_ri.sh
+++ scripts/test_ri.sh
@@ -10,7 +10,7 @@ mkdir -p conf/dev
 echo TESTME_KEY=$TESTME_KEY
 
 set -e
-openssl aes-256-cbc -d -a -in ../lergo-build/scripts/testMe.json.enc -out conf/dev/me.json -pass pass:$TESTME_KEY
+openssl aes-256-cbc -d -a -in ../lergo-build/scripts/testMe.json.enc -out conf/dev/me.json -pass pass:$TESTME_KEY -md md5
 ./node_modules/.bin/grunt testBefore
 ./node_modules/.bin/grunt
 ./node_modules/.bin/grunt testAfter
